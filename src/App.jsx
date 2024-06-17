087d27bb7f18cdec704a8ad6942deb3858ed39f8
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-06-17 22:09:03 +0530

    Imports and Dependencies React Import: The code imports the React library, which is necessary to create React components. React Router Imports: createBrowserRouter: This function from react-router-dom is used to create a router instance for handling client-side navigation. RouterProvider: This component is used to provide the router instance to the React application. Component Imports: Home: The Home component is imported from the ./components/HomePage file. VideoPage: The VideoPage component is imported from the ./components/VideoPage file.

---
+++ src/App.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+import { createBrowserRouter,RouterProvider } from 'react-router-dom'
+import Home from './components/HomePage'
+import VideoPage from './components/VideoPage'
+
+
+
