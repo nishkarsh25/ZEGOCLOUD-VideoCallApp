6a756aa4de4ec45e85b619450ba39b27f71dc046
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-06-17 22:01:35 +0530

    Variables roomID: The roomID variable is assigned the value of id extracted from the URL parameters.

--- src/components/VideoPage.jsx
+++ src/components/VideoPage.jsx
@@ -3,7 +3,7 @@ import { useParams } from "react-router-dom";
 import { ZegoUIKitPrebuilt } from "@zegocloud/zego-uikit-prebuilt";
 
 const VideoPage = () => {
-  
+  const { id } = useParams();
 
   
 };
