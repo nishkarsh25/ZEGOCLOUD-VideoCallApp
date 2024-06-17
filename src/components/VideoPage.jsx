61e03a711e0c2cfd11a7e032ac35da948a9913d4
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-06-17 22:01:58 +0530

    Asynchronous Function (myMeeting) Function Definition: The function myMeeting is defined as an asynchronous function and takes a single argument element, which is expected to be a DOM element. Environment Variables: appID: Extracted from the environment variables using import.meta.env.VITE_APPID and converted to a number. serverSecret: Also extracted from the environment variables using import.meta.env.VITE_SERVERSECRET. Kit Token Generation: The generateKitTokenForTest method from ZegoUIKitPrebuilt is used to generate a kit token. This method requires the following parameters: appID: The application ID. serverSecret: A secret key for the server. roomID: The room ID. A unique user ID, generated using Date.now().toString() to ensure uniqueness. A default user name, here set as "default User". Instance Creation: ZegoUIKitPrebuilt.create(kitToken) is called to create an instance of Zego's prebuilt UI kit using the generated kit token. Join Room: The joinRoom method on the instance (zp) is called with an options object: container: The DOM element where the video call interface will be rendered. sharedLinks: An array containing objects that define shareable links. In this case, a single link is provided which includes: name: A label for the link, "Copy link". url: A URL constructed using the current protocol, host, and pathname, appended with the query parameter roomID. scenario: Specifies the mode of the call. Here it is set to ZegoUIKitPrebuilt.OneONoneCall for a 1-on-1 call.

--- src/components/VideoPage.jsx
+++ src/components/VideoPage.jsx
@@ -5,6 +5,43 @@ import { ZegoUIKitPrebuilt } from "@zegocloud/zego-uikit-prebuilt";
 const VideoPage = () => {
   const { id } = useParams();
 
+  const roomID = id;
+  let myMeeting = async (element) => {
+    // generate Kit Token
+    const appID = Number(import.meta.env.VITE_APPID);
+    console.log(appID);
+    const serverSecret = import.meta.env.VITE_SERVERSECRET;
+    const kitToken = ZegoUIKitPrebuilt.generateKitTokenForTest(
+      appID,
+      serverSecret,
+      roomID,
+      Date.now().toString(),
+      "default User"
+    );
+
+    // Create instance object from Kit Token.
+    const zp = ZegoUIKitPrebuilt.create(kitToken);
+    // start the call
+    zp.joinRoom({
+      container: element,
+      sharedLinks: [
+        {
+          name: "Copy link",
+          url:
+            window.location.protocol +
+            "//" +
+            window.location.host +
+            window.location.pathname +
+            "?roomID=" +
+            roomID,
+        },
+      ],
+      scenario: {
+        mode: ZegoUIKitPrebuilt.OneONoneCall, // To implement 1-on-1 calls, modify the parameter here to [ZegoUIKitPrebuilt.OneONoneCall].
+      },
+    });
+  };
+
   
 };
 
