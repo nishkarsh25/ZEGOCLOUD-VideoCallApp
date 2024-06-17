5a748759b3865609f31a97130ff477fef0d52fd9
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-06-17 22:09:12 +0530

    Component Definition Component Name: The functional component is named App.

--- src/App.jsx
+++ src/App.jsx
@@ -3,5 +3,10 @@ import { createBrowserRouter,RouterProvider } from 'react-router-dom'
 import Home from './components/HomePage'
 import VideoPage from './components/VideoPage'
 
+const App = () => {
+  
+  
+  
+}
 
 
